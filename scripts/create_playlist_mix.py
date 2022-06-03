884602558cdaeb6efba54f49149843acc5956328
Alberto Redondo <albertomost@gmail.com>
2022-06-04 01:57:58 +0200

    Add first scripts

---
+++ scripts/create_playlist_mix.py
@@ -0,0 +1,90 @@
+# Script that creates a Spotify playlist with random tracks from other playlists
+# If there are duplicates in the resulting playlist, they are removed
+# Requires: spotipy
+# Set SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET environment variables
+# Set SPOTIPY_REDIRECT_URI environment variable (e. g. to http://localhost:9090)
+
+import spotipy
+import random
+import time
+from spotipy.oauth2 import SpotifyOAuth
+
+# Define needed scopes:
+# * playlist-read-collaborative: to get tracks from collab playlists
+# * playlist-read-private: to get tracks from private playlists
+# * playlist-modify-private: to create the playlist and add tracks to it
+# * user-library-read: to get tracks from library (liked songs)
+scope = "playlist-read-collaborative playlist-read-private playlist-modify-private user-library-read"
+
+# Set up auth using Authorization Code Flow
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+user = ""  # Replace with user ID (username)
+
+# Define playlist IDs as variables
+playlist_temporada_id = ""
+playlist_temporada_plus_id = ""
+
+# Name of the new playlist
+new_playlist_name = "Shuffle mix — " + time.strftime("%d/%m/%y")
+
+# List of dictionaries to define playlists:
+# * playlist_id (string): ID of the playlist ("saved" for saved tracks)
+# * count (int): number of tracks to add from the playlist
+playlists = [
+    {
+        "playlist_id": "saved",
+        "count": 50
+    },
+    {
+        "playlist_id": playlist_temporada_id,
+        "count": 20
+    },
+    {
+        "playlist_id": playlist_temporada_plus_id,
+        "count": 30
+    }
+]
+
+# Create the new playlist and store its ID in a variable
+new_playlist_id = sp.user_playlist_create(user, new_playlist_name, public=False)['id']
+
+new_playlist_song_ids = []  # List for the songs of the new playlist
+
+# Iterate over the playlists
+for playlist in playlists:
+    playlist_id = playlist['playlist_id']
+    count = playlist['count']
+
+    offset = 0  # Keep an offset bc requests have a 50/100 track limit
+    song_ids = []  # List for the songs from each playlist
+
+    # If playlist_id is "saved", request saved tracks; otherwise, request tracks from the corresponding playlist
+    if playlist_id == "saved":
+        items = sp.current_user_saved_tracks(limit=50)['items']  # Max limit for saved tracks = 50
+    else:
+        items = sp.playlist_items(playlist_id, limit=100)['items']  # Max limit for playlists = 100
+
+    # Iterate while getting items from the request
+    while len(items) > 0:
+        for item in items:
+            song_ids.append(item['track']['id'])  # Append the ID of the track
+
+        # Request next 50/100 items
+        if playlist_id == "saved":
+            offset += 50
+            items = sp.current_user_saved_tracks(limit=50, offset=offset)['items']
+        else:
+            offset += 100
+            items = sp.playlist_items(playlist_id, limit=100, offset=offset)['items']
+
+    # Shuffle tracks, take count and add them to the list of songs of the new playlist
+    random.shuffle(song_ids)
+    new_playlist_song_ids.extend(song_ids[:count])
+
+# Remove duplicates from the list of songs and shuffle
+new_playlist_song_ids = list(dict.fromkeys(new_playlist_song_ids))
+random.shuffle(new_playlist_song_ids)
+
+# Add tracks to the new playlist
+sp.playlist_add_items(new_playlist_id, new_playlist_song_ids)
---
+++ scripts/get_playlist_tracks.py
@@ -0,0 +1,46 @@
+# Script that prints all tracks from a Spotify playlist
+# Requires: spotipy
+# Set SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET environment variables
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Set up auth using Client Credentials Flow
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+playlist_id = ""  # Replace with playlist ID
+
+playlist = sp.playlist(playlist_id)  # Load playlist info
+items = sp.playlist_items(playlist_id, limit=100)['items']  # Request first 100 items
+
+track_total = playlist['tracks']['total']  # Number of tracks in the playlist
+offset = 0  # Keep an offset bc requests have a 100 track limit
+
+# Print playlist name and number of tracks
+print(playlist['name'] + " — " + str(track_total) + " tracks\n")
+
+# Iterate while getting items from the request
+while len(items) > 0:
+    for item in items:
+        track = item['track']
+        artists = track['artists']
+
+        # If just one artist, print its name
+        if len(artists) == 1:
+            print(artists[0]['name'] + " — ", end="")
+        # If more than one artist, separate them with commas
+        else:
+            for artist in artists[:-1]:
+                print(artist['name'], end=", ")
+
+            print(artists[len(artists) - 1]['name'] + " — ", end="")
+
+        # Print track name
+        print(track['name'])
+
+    # Increment offset in 100 (max limit)
+    offset += 100
+
+    # Request next 100 items
+    items = sp.playlist_items(playlist_id, limit=100, offset=offset)['items']
