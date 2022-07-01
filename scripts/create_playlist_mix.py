400e265bfebad36fdf22e25eaabf89df4e4763f4
Alberto Redondo <albertomost@gmail.com>
2022-07-01 20:24:11 +0200

    New feature: add all songs in a playlist if count < 0 (e. g. -1)

--- scripts/create_playlist_mix.py
+++ scripts/create_playlist_mix.py
@@ -53,6 +53,10 @@ for playlist in playlists:
     playlist_id = playlist['playlist_id']
     count = playlist['count']
 
+    # If count < 0, use all songs
+    if count < 0:
+        count = sp.playlist(playlist_id)['tracks']['total']  # Get number of songs
+
     offset = 0  # Keep an offset bc requests have a 50/100 track limit
     song_ids = []  # List for the songs from each playlist
 
